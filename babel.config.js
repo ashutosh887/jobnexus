307382100df36d1829bcf42828e66ff23f230454
Ashutosh Jha <ashutoshj887@gmail.com>
2023-11-21 00:31:34 +0530

    add: env support

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,19 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // Required for expo-router
       'expo-router/babel',
+      ['module:react-native-dotenv', {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        blacklist: null, // DEPRECATED
+        whitelist: null, // DEPRECATED
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      }],
     ],
   };
 };
