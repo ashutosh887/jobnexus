a1bd3a09cc9de47c61fa40e6030093dfc5fcc017
Ashutosh Jha <ashutoshj887@gmail.com>
2023-11-24 16:23:22 +0530

    feat: about screen

--- app/about/index.tsx
+++ app/about/index.tsx
@@ -1,8 +1,22 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  Linking,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
 import { Stack, useRouter } from "expo-router";
-import { COLORS, SIZES, icons } from "../../config";
+import { COLORS, FONT, SIZES, icons } from "../../config";
 import ScreenHeaderBtn from "../../components/common/ScreenHeaderBtn";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { githubURL } from "../../config/constants";
+
+const { height, width } = Dimensions.get("window");
 
 export default function About() {
   const router = useRouter();
@@ -32,7 +46,19 @@ export default function About() {
       />
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.container}>
-          <Text>About</Text>
+          <Image source={icons.placeholder} style={styles.logo} />
+          <Text style={styles.title}>JobNexus</Text>
+          <Text style={styles.description}>
+            A React Native Mobile Application to checkout job listings across
+            the web.
+          </Text>
+
+          <TouchableOpacity
+            style={styles.link}
+            onPress={() => Linking.openURL(githubURL)}
+          >
+            <Text style={styles.linkBtn}>Developed by @ashutosh887</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -42,9 +68,39 @@ export default function About() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
     padding: SIZES.medium,
     paddingBottom: 100,
+    height: height - 40,
+  },
+  logo: {
+    height: 160,
+    width: 160,
+    borderRadius: 80,
+    marginTop: 16,
+  },
+  title: {
+    fontSize: SIZES.large,
+    fontFamily: FONT.bold,
+    margin: 16,
+  },
+  description: {
+    fontSize: SIZES.medium,
+    fontFamily: FONT.regular,
+    textAlign: "center",
+  },
+  link: {
+    backgroundColor: "#FE7654",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: SIZES.medium,
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+    marginTop: 36,
+  },
+  linkBtn: {
+    fontSize: SIZES.medium,
+    color: COLORS.white,
+    fontFamily: FONT.bold,
   },
 });
--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,5 @@
 export const appName = "JobNexus";
 
 export const userName = "DevSapien";
+
+export const githubURL = "https://github.com/ashutosh887";
